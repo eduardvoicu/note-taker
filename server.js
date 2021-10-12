74d2b51b12f50e3a8918888e9fa59511d9745945
Eduard Voicu <iameduardvoicu@gmail.com>
2021-10-12 11:41:44 -0500

    creating consts in server js file and connecting files to other files

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const database = require('./db/db.json')
+const database = require('./db/db.json');
+const uid = require('./helper/uid'); // attaching a method that helps create unique numbers for ID
+//express package functionality and designates a PORT to run application
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// all static files in "public" folder will be served
+app.use(express.static('public'));
